5955a5a9d77305f9528494bb9a0e83e4b355339a
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-13 17:21:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,3 +21,10 @@ def position_taken?(board, position)
     true
   end
 end
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+      true
+    else
+      false
+  end
+end
